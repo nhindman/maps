b78787857697c3accb63aa7ac1998bb4cab1067c
Daniel Chao <daniel.h.chao@gmail.com>
2014-01-15 17:32:01 -0800

    example time

--- public/js/main.js
+++ public/js/main.js
@@ -1,33 +1,22 @@
 define(function(require, exports, module) {
-    var FamousEngine = require('famous/Engine');
-    var App = require('app/App');
+  var FamousEngine = require('famous/Engine');
+  var App = require('app/App');
 
-    var Surface = require('famous/Surface');
 
-    // create the App from the template
-    var myApp = new App();
+  // create the App from the template
+  var myApp = new App();
 
-    // create a display context and hook in the App
-    var mainDisplay = FamousEngine.createContext();
-    mainDisplay.link(myApp);
-    FamousEngine.pipe(myApp);
+  // create a display context and hook in the App
+  var mainDisplay = FamousEngine.createContext();
+  mainDisplay.link(myApp);
+  FamousEngine.pipe(myApp);
 
-    // create a section
-    var mySection = myApp.section('main');
-    mySection.setOptions({
-        title: 'Main',
-        navigation: {caption: 'Main', icon: '@'}
-    });
-    
-    // create an example surface 
-    var mySurface = new Surface({
-        properties: { 'padding': '20px' },
-        content: '<h2>Hello World</h2><p>Lorem ipsum sit dolor amet &hellip;</p>'
-    });
+  // create the various sections
+  require('first')(myApp);
+  require('second')(myApp);
 
-    // link the surface into the setion
-    mySection.link(mySurface);
+  // start on the main section
+  myApp.select('main');
 
-    // start on the main section
-    myApp.select('main');
+
 });
---
+++ public/js/second.js
@@ -0,0 +1,20 @@
+define(function(require, exports, module){
+  var Surface = require('famous/Surface');
+
+  module.exports = function(myApp){
+    var surface2 = new Surface({
+      properties: {
+        'padding': '20px'
+      },
+      content: '<div id="hi">Hi how are you</div>'
+    });
+    var section2 = myApp.section('second');
+    section2.setOptions({
+      title: 'second',
+      navigation: {
+        caption: 'second', icon: '$'
+      }
+    });
+    section2.link(surface2);
+  }
+})
