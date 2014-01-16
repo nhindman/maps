6b9c75eb1fd9ca31b87c80c13cbc36a796e10864
Nick Loveridge <nick@fusemedia.ca>
2014-01-15 16:09:49 -0800

    added yelp API interface and basic geolocation endpoint

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+
+  port : 3000,
+
+  yelp : {
+    consumerKey:     "J12EbLbP1ZcM1EABGwgF-w", 
+    consumerSecret:  "gFdsTIoEmwHwHAPLrA7Q8LRWNrQ",
+    token:           "ui35POX19Xw_sL-rF87d6NBuDwTXPk4M",
+    tokenSecret:     "97_vMXbJRY0MUl1SRabqBl-hFXE"
+  }
+
+};
---
+++ server.js
@@ -0,0 +1,50 @@
+var config  = require('./config.js');
+var express = require('express');
+var url     = require('url');
+var yelp    = require('./yelp').createClient({
+  consumer_key: config.yelp.consumerKey, 
+  consumer_secret: config.yelp.consumerSecret,
+  token: config.yelp.token,
+  token_secret: config.yelp.tokenSecret
+});
+
+var app = express.createServer();
+
+// Yelp API endpoints
+// FIXME: Not stuck on using 'yelp' in endpoint name
+app.get('/yelp/geo', function(req, res) {
+
+  // Get the url query parameters
+  var query = url.parse(req.url, true).query;
+
+  // Provide default values
+  var params = {
+    lat: query.lat || 37.788022,
+    long: query.long || -122.399797,
+    filter: query.filter || 'arts'
+  }
+
+  yelp.geo(params, function(error, data) {
+    if(error) {
+      console.log('Error:', error);
+    } else {
+      
+      // Loop through all listed businesses
+      // for(var i = 0; i < data.businesses.length; i++) {
+      //   console.log(data.businesses[i].name);
+      //   console.log(data.businesses[i].location);
+      // }
+
+      res.send(data.businesses);
+    }
+  });
+  
+});
+
+// Serve every static file in public dir
+app.use(express.static(__dirname + "/public"));
+
+app.listen(config.port);
+console.log('Listening on port ' + config.port);
+
+
