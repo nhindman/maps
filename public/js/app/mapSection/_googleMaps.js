fb897a95743d0b606ee07b82fb60d1d5cadd520a
Daniel Chao <daniel.h.chao@gmail.com>
2014-01-21 16:30:20 -0800

    moved mapstyle to its own file

--- public/js/app/mapSection/_googleMaps.js
+++ public/js/app/mapSection/_googleMaps.js
@@ -8,139 +8,9 @@ define(function(require, exports, module){
 
   require('../../../lib/requirejs-plugins/src/async!https://maps.googleapis.com/maps/api/js?key=AIzaSyCUK_sH0MT-pkWbyBGJe-XoJ_kldSde81o&sensor=true');
 
-  var mapBox = [
-    {
-      "featureType": "water",
-      "stylers": [
-        {
-          "saturation": 43
-        },
-        {
-          "lightness": -11
-        },
-        {
-          "hue": "#0088ff"
-        }
-      ]
-    },
-    {
-      "featureType": "road",
-      "elementType": "geometry.fill",
-      "stylers": [
-        {
-          "hue": "#ff0000"
-        },
-        {
-          "saturation": -100
-        },
-        {
-          "lightness": 99
-        }
-      ]
-    },
-    // {
-    //   "featureType": "road",
-    //   "elementType": "geometry.stroke",
-    //   "stylers": [
-    //     {
-    //       "color": "#808080"
-    //     },
-    //     {
-    //       "lightness": 54
-    //     }
-    //   ]
-    // },
-    {
-      "featureType": "landscape.man_made",
-      "elementType": "geometry.fill",
-      "stylers": [
-        {
-          "color": "#ece2d9"
-        }
-      ]
-    },
-    {
-      "featureType": "poi.park",
-      "elementType": "geometry.fill",
-      "stylers": [
-        {
-          "color": "#ccdca1"
-        }
-      ]
-    },
-    {
-      "featureType": "road",
-      "elementType": "labels.text.fill",
-      "stylers": [
-        {
-          "color": "#767676"
-        }
-      ]
-    },
-    {
-      "featureType": "road",
-      "elementType": "labels.text.stroke",
-      "stylers": [
-        {
-          "color": "#ffffff"
-        }
-      ]
-    },
-    {
-      "featureType": "poi",
-      "stylers": [
-        {
-          "visibility": "off"
-        }
-      ]
-    },
-    {
-      "featureType": "landscape.natural",
-      "elementType": "geometry.fill",
-      "stylers": [
-        {
-          "visibility": "on"
-        },
-        {
-          "color": "#b8cb93"
-        }
-      ]
-    },
-    {
-      "featureType": "poi.park",
-      "stylers": [
-        {
-          "visibility": "on"
-        }
-      ]
-    },
-    {
-      "featureType": "poi.sports_complex",
-      "stylers": [
-        {
-          "visibility": "on"
-        }
-      ]
-    },
-    {
-      "featureType": "poi.medical",
-      "stylers": [
-        {
-          "visibility": "on"
-        }
-      ]
-    },
-    {
-      "featureType": "poi.business",
-      "stylers": [
-        {
-          "visibility": "simplified"
-        }
-      ]
-    }
-  ]
-
   module.exports = function(mapSection, cards){
+
+
 
     var queryRadius = 3000 // meters
 
@@ -261,7 +131,7 @@ define(function(require, exports, module){
         zoom: 15,
         disableDefaultUI: true,
         disableDoubleClickZoom: true,
-        styles: mapBox
+        styles: require('app/mapSection/_mapStyle')()
       };
 
       map = new google.maps.Map(document.getElementById('map-canvas'),
